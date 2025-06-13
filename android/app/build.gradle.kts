a8d31c4896f1335f8e776a1a9293fdee9b7c741d
LilSeabass08 <sashi.ethington@gmail.com>
2025-06-13 14:32:34 -0600

    updated target sdk version to match latest update and changed the test file to better run tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.barcode_scanner"
+    testNamespace = "com.example.barcode_scanner.test"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -25,7 +26,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
